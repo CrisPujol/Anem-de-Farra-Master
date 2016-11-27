11168f609ca086b0537030353a48465244414788
CrisPujol <cris.pujol.busquet@gmail.com>
2016-11-27 10:54:27 +0100

    register firsts steps

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 const db = require('./db');
 const path = require('path');
 const passport = require('passport')
+var http = require('http');
 
 const LocalStrategy = require('passport-local').Strategy;
 const logger = require('morgan');
@@ -23,17 +24,26 @@ if ( thereIsDotEnv ) require('dotenv').load()
 
 const app = express();
 
+
+// passport config
+
 app.set('view engine', 'pug')
 app.use( express.static("public") )
 app.use( bodyparser.urlencoded( { extended: false } ) )
+
+app.use( logger('dev') );
 
 app.use( cookieParser() );
 app.use( session({ secret: 'supersecretworddonottelltoanyone'}) );
 app.use( flash() );
 
+app.use( passport.initialize() );
+app.use( passport.session() );
+
+
+// GET AND FILTER FEASTS
 app.use( prepareParams )
 	
-	// app.get('/', (req,res) => res.render('index') )
 	app.get('/', getFeasts )
 	app.get('/feasts', getFeasts )
 	app.post('/feasts', getFeastFiltered )
@@ -41,4 +51,60 @@ app.use( prepareParams )
 	app.get('/feast/:id', getFeastbyId )
 
 
+
+
+//PASSPORT
+const AUTH = process.env.AUTH || 'local';
+
+if (AUTH === 'local') {
+	/* @begin LOCAL */
+	var Account = require('./models/account');
+	passport.use( new LocalStrategy( Account.authenticate() ) );
+	passport.serializeUser( Account.serializeUser() );
+	passport.deserializeUser( Account.deserializeUser() );
+
+	const routerAuthLocal = require('./routes/auth/local')
+	app.use('/local', routerAuthLocal)
+	/* @end LOCAL */
+}
+
+
+
+
+
+// // routes
+
+app.get('/', function (req, res) {
+	const user = req.user;
+	const auth_method = AUTH;
+  res.render('index', { user, auth_method });
+});
+
+
+app.get('/account', isAuthenticated, (req, res) => {
+
+	const userId = req.session.passport.user;
+	const message = req.flash('message');
+
+	User.findById( userId )
+		.then( user => res.render( 'account', { user, message } ) )
+		.catch( console.log )
+});
+
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+
+
+// // test authentication
+function isAuthenticated(req, res, next) {
+  if ( req.isAuthenticated() ) return next();
+  res.redirect('/');
+}
+
 app.listen(3000, () => console.log("Listening on port 3000..."))
+
+module.exports = app;
